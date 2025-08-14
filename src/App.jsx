1b0fa069dbe340af023d1140042b5634340e2a4d
pauljosephangco <paulangco1@gmail.com>
2025-08-14 11:10:26 +0800

    change dollar sign to PHP sign

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
             key={index}
             image={product.image}
             name={product.name}
-            price={`₱${product.price.toLocaleString()}`} 
+            price={`₱{product.price.toLocaleString()}`} 
             description={product.description}
           />
         ))}
